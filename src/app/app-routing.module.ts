86d1414b2863251e311f86ed134c2d2b802b5daf
Efren Rodriguez <efren.rodriguez@protexa.com.mx>
2020-03-18 15:04:47 -0600

    [change] se cambia el ruteo de las secciones, se agregan /symptoms /heat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './sections/home/home.component';
+import {SymptomsComponent} from './sections/symptoms/symptoms.component';
+import {HeatmapComponent} from './sections/heatmap/heatmap.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'symptoms',      component: SymptomsComponent },
+  { path: 'map',      component: HeatmapComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

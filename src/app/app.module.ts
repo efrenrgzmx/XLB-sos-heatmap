8e5741f58625c0023f71bb07608faf82d700759b
Efren Rodriguez <efren.rodriguez@protexa.com.mx>
2020-03-23 13:10:38 -0600

    [add] home super básico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './sections/home/home.component';
 import { HeatmapComponent } from './sections/heatmap/heatmap.component';
 import { SymptomsComponent } from './sections/symptoms/symptoms.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SymptomsComponent } from './sections/symptoms/symptoms.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
